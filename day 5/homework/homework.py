a57abd39ed50cd07d9d25a5e2663bbed1a27bfec
Andriajaneza <andriajazneza@gmail.com>
2024-07-08 23:12:43 -0700

    goa

---
+++ day 5/homework/homework.py
@@ -0,0 +1,37 @@
+num1 = 20
+
+num2 = 30
+
+total = num1 + num2
+
+print (total)
+
+greeting = "hello"
+age = 11
+message = (greeting + ", i am " + str(age) + " years old.")
+print(message)
+
+
+a = 7
+b = 9
+result = a * b
+print ("result of division is: " + str(result))
+
+
+first_name = "john"
+last_name = "doe"
+print (first_name + " " + last_name)
+
+greeting = "hello"
+name = "world"
+full_message = greeting + "" + name
+print (full_message)
+
+a = 134
+b = 2345
+result = a+b
+print (result)
+
+man = "lazy"
+bananas = 5
+print (man + " has " + str(bananas) + " bananas.")
