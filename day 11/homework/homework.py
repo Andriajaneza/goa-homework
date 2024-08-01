40b510fa238748ca56d8255e7b3681628b64d17f
Andriajaneza <andriajazneza@gmail.com>
2024-08-01 01:16:18 -0700

    day 11

---
+++ day 11/homework/homework.py
@@ -0,0 +1,4 @@
+cocacola = int(input("coca_cola_value : "))
+kampoti = int(input("babushkas_kampoti : "))
+
+print  (cocacola < kampoti)
---
+++ day 11/homework/homework2.py
@@ -0,0 +1,7 @@
+num_1 = 7
+num_2 = 12
+
+print(num_1 < num_2)
+print(num_1 > num_2)
+print(num_2 > num_1)
+print(num_2 < num_1)
